613aba8dcfac374dc939c9cd0a421c10fa6f97d6
SpearJin <ckdwls2525@gmail.com>
2022-01-24 17:29:59 +0900

    이미지 가운데 정렬하기

--- src/components/slide/slide.jsx
+++ src/components/slide/slide.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import './slide.css';
 
 const slideImg = [
+  // 가짜 데이터
+  {
+    id: -1,
+    image: 'https://static.wanted.co.kr/images/banners/1473/41f7b36e.thumb_1006_280.jpg',
+    title: '개발자가 되고싶은 분들!?',
+    subTitle: '프론트앤드 무료 교육과정 참여하기',
+  },
+  {
+    id: -2,
+    image: 'https://static.wanted.co.kr/images/banners/1484/b2853456.thumb_1006_280.jpg',
+    title: '성장하는 개발자가 되려면?',
+    subTitle: '000 검색하지 말 것!',
+  },
+
+  // 진짜 데이터
   {
     id: 0,
     image: 'https://static.wanted.co.kr/images/banners/1460/619f3af7.thumb_1006_280.jpg',
@@ -37,14 +53,45 @@ const slideImg = [
     title: '성장하는 개발자가 되려면?',
     subTitle: '000 검색하지 말 것!',
   },
+
+  // 가짜 데이터
+  {
+    id: 6,
+    image: 'https://static.wanted.co.kr/images/banners/1460/619f3af7.thumb_1006_280.jpg',
+    title: '개발자 성장 비결 공개!',
+    subTitle: 'Velog, 글 쓰는 개발자들의 이야기',
+  },
+  {
+    id: 7,
+    image: 'https://static.wanted.co.kr/images/banners/1486/fba2df30.thumb_1006_280.jpg',
+    title: '성과를 내는 마케팅',
+    subTitle: '실제 사례를 공개 합니다!',
+  },
 ];
 
 const Slide = (props) => {
+  const [currentIndex, setCurrentIndex] = useState(2);
+  const [slideWidth, setSlideWidth] = useState(0);
+
+  const innerWidth = window.innerWidth;
+  const slideItem = useRef(null);
+
+  useEffect(() => {
+    setSlideWidth(slideItem.current.getBoundingClientRect().width);
+  });
+
   return (
-    <div>
-      {slideImg.map((img) => (
-        <img key={img.id} src={img.image} alt='img' />
-      ))}
+    <div className='slide'>
+      <ul
+        className='slide_list'
+        style={{ transform: `translateX(calc(${(innerWidth - slideWidth) / 2}px - ${slideWidth * currentIndex}px))` }}
+      >
+        {slideImg.map((img) => (
+          <li key={img.id} className='slide_item' ref={slideItem}>
+            <img className='slide-img' src={img.image} alt='img' />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
