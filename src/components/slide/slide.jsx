23fe643c1e506ba4afb9048698503c7f12df05e4
SpearJin <ckdwls2525@gmail.com>
2022-02-04 15:41:19 +0900

    slide_swiper mousemove 구현

--- src/components/slide/slide.jsx
+++ src/components/slide/slide.jsx
@@ -76,17 +76,15 @@ const Slide = (props) => {
   const innerWidth = window.innerWidth;
   const slideList = useRef(null);
   const slideItem = useRef(null);
+
   let intervalTime = useRef(null);
+  let setTime = useRef(null);
+  let isMove = useRef(false);
 
   useEffect(() => {
     setSlideWidth(slideItem.current.getBoundingClientRect().width);
-    clearInterval(intervalTime.current);
     intervalImage();
-
-    // console.log(slideList.current.children);
-    // const slideImages = [...slideList.current.children];
     const slideImages = slideList.current.querySelectorAll('.slide_item');
-
     slideImages.forEach((item, index) => {
       if (currentIndex === index) {
         item.classList.add('active');
@@ -97,6 +95,7 @@ const Slide = (props) => {
   }, [currentIndex]);
 
   const intervalImage = () => {
+    clearInterval(intervalTime.current);
     intervalTime.current = setInterval(() => {
       let nextIndex;
       nextIndex = currentIndex + 1;
@@ -104,13 +103,17 @@ const Slide = (props) => {
     }, 2000);
   };
 
-  // 마우스 올릴때
-  const handlerMouseOut = () => {
+  // 마우스 땔떼
+  const handlerMouseLeave = (e) => {
+    if (startPostion) {
+      handlerMouseUp(e);
+    }
     intervalImage();
   };
 
-  // 마우스 땔떼
+  // 마우스 올릴때
   const handlerMouseOver = () => {
+    console.log('mouseOver');
     clearInterval(intervalTime.current);
   };
 
@@ -121,13 +124,50 @@ const Slide = (props) => {
   };
 
   const moveImage = (nextIndex) => {
-    slideList.current.style.transition = '200ms';
+    if (isMove.current) {
+      return;
+    }
+    isMove.current = true;
+    slideList.current.style.transition = '300ms';
+    slideList.current.style.pointerEvents = 'none';
     setCurrentIndex(nextIndex);
-    setTimeout(() => {
+    clearTimeout(setTime.current);
+    setTime.current = setTimeout(() => {
       slideList.current.style.transition = '0s';
+      slideList.current.style.pointerEvents = 'all';
       nextIndex = nextIndex < 2 ? slideImg.length - 3 : nextIndex === slideImg.length - 2 ? 2 : nextIndex;
       setCurrentIndex(nextIndex);
-    }, 200);
+      isMove.current = false;
+    }, 300);
+  };
+
+  let startPostion = 0;
+  let endPostion = 0;
+  let swiperX = 0;
+  let swiperWidth = 0;
+
+  const handlerMouseDown = (e) => {
+    console.log('mouseDown');
+    e.preventDefault();
+    startPostion = e.clientX;
+    document.onmousemove = mouse;
+  };
+
+  const handlerMouseUp = (e) => {
+    endPostion = e.clientX;
+    console.log(startPostion, endPostion);
+    document.onmousemove = null;
+    let nextIndex;
+    nextIndex = startPostion > endPostion ? currentIndex + 1 : currentIndex - 1;
+    moveImage(nextIndex);
+  };
+
+  const mouse = (e) => {
+    swiperX = e.clientX;
+    swiperWidth = startPostion - swiperX;
+    slideList.current.style.transform = `translateX(calc(${(innerWidth - slideWidth) / 2}px - ${
+      slideWidth * currentIndex
+    }px - ${swiperWidth}px))`;
   };
 
   return (
@@ -136,8 +176,10 @@ const Slide = (props) => {
         className='slide_list'
         ref={slideList}
         style={{ transform: `translateX(calc(${(innerWidth - slideWidth) / 2}px - ${slideWidth * currentIndex}px))` }}
+        onMouseDown={handlerMouseDown}
+        onMouseUp={handlerMouseUp}
         onMouseOver={handlerMouseOver}
-        onMouseOut={handlerMouseOut}
+        onMouseLeave={handlerMouseLeave}
       >
         {slideImg.map((img) => (
           <li key={img.id} className='slide_item' ref={slideItem}>
